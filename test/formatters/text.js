6762262e83fdbee15ae217a0ec86df41fec6b70b
Cliffano Subagio <cliffano@gmail.com>
2013-04-30 14:53:34 +1000

    Add unit tests for setup element having name field.

--- test/formatters/text.js
+++ test/formatters/text.js
@@ -7,18 +7,23 @@ buster.testCase('text - format', {
     assert.equals(result.length, 1);
     assert.equals(result[0], 'OK | http://somehost - somedesc');
   },
-  'should exclude desc from text when it does not exist': function () {
+  'should format text properly when name exists': function () {
+    var result = text.format([{ uri: 'http://somehost', status: 'OK', name: 'someapp' }]);
+    assert.equals(result.length, 1);
+    assert.equals(result[0], 'OK | someapp - http://somehost');
+  },
+  'should exclude name and desc from text when they do not exist': function () {
     var result = text.format([{ uri: 'http://somehost', status: 'OK' }]);
     assert.equals(result.length, 1);
     assert.equals(result[0], 'OK | http://somehost');
   },
   'should format multiple texts properly': function () {
     var result = text.format([
-      { uri: 'http://somehost1', status: 'OK', desc: 'somedesc1' },
-      { uri: 'http://somehost2', status: 'FAIL', desc: 'somedesc2' }
+      { name: 'someapp1', uri: 'http://somehost1', status: 'OK', desc: 'somedesc1' },
+      { name: 'someapp2', uri: 'http://somehost2', status: 'FAIL', desc: 'somedesc2' }
     ]);
     assert.equals(result.length, 2);
-    assert.equals(result[0], 'OK | http://somehost1 - somedesc1');
-    assert.equals(result[1], 'FAIL | http://somehost2 - somedesc2');
+    assert.equals(result[0], 'OK | someapp1 - http://somehost1 - somedesc1');
+    assert.equals(result[1], 'FAIL | someapp2 - http://somehost2 - somedesc2');
   }
 });
--- test/formatters/xml.js
+++ test/formatters/xml.js
@@ -6,15 +6,19 @@ buster.testCase('text - format', {
     var result = xml.format([{ uri: 'http://somehost', status: 'OK', desc: 'somedesc' }]);
     assert.equals(result, '<?xml version="1.0" encoding="UTF-8"?>\n<results>\n  <item>\n    <uri>http://somehost</uri>\n    <status>OK</status>\n    <desc>somedesc</desc>\n  </item>\n</results>\n');
   },
-  'should exclude desc from xml when it does not exist': function () {
+  'should format xml properly when name exists': function () {
+    var result = xml.format([{ uri: 'http://somehost', status: 'OK', name: 'someapp' }]);
+    assert.equals(result, '<?xml version="1.0" encoding="UTF-8"?>\n<results>\n  <item>\n    <uri>http://somehost</uri>\n    <status>OK</status>\n    <name>someapp</name>\n  </item>\n</results>\n');
+  },
+  'should exclude name and desc from xml when they do not exist': function () {
     var result = xml.format([{ uri: 'http://somehost', status: 'OK' }]);
     assert.equals(result, '<?xml version="1.0" encoding="UTF-8"?>\n<results>\n  <item>\n    <uri>http://somehost</uri>\n    <status>OK</status>\n  </item>\n</results>\n');
   },
   'should format xml with multiple results properly': function () {
     var result = xml.format([
-      { uri: 'http://somehost1', status: 'OK', desc: 'somedesc1' },
-      { uri: 'http://somehost2', status: 'FAIL', desc: 'somedesc2' }
+      { name: 'someapp1', uri: 'http://somehost1', status: 'OK', desc: 'somedesc1' },
+      { name: 'someapp2', uri: 'http://somehost2', status: 'FAIL', desc: 'somedesc2' }
     ]);
-    assert.equals(result, '<?xml version="1.0" encoding="UTF-8"?>\n<results>\n  <item>\n    <uri>http://somehost1</uri>\n    <status>OK</status>\n    <desc>somedesc1</desc>\n  </item>\n  <item>\n    <uri>http://somehost2</uri>\n    <status>FAIL</status>\n    <desc>somedesc2</desc>\n  </item>\n</results>\n');
+    assert.equals(result, '<?xml version="1.0" encoding="UTF-8"?>\n<results>\n  <item>\n    <name>someapp1</name>\n    <uri>http://somehost1</uri>\n    <status>OK</status>\n    <desc>somedesc1</desc>\n  </item>\n  <item>\n    <name>someapp2</name>\n    <uri>http://somehost2</uri>\n    <status>FAIL</status>\n    <desc>somedesc2</desc>\n  </item>\n</results>\n');
   }
 });
