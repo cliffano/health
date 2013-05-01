2ed480ee2f3bf4f597de793dce6a348fa696e667
Cliffano Subagio <cliffano@gmail.com>
2013-05-01 23:02:01 +1000

    Add html formatter.

---
+++ test/formatters/html.js
@@ -0,0 +1,14 @@
+var buster = require('buster'),
+  fs = require('fs'),
+  text = require('../../lib/formatters/html');
+
+buster.testCase('html - format', {
+  setUp: function () {
+    this.mockFs = this.mock(fs);
+  },
+  'should format text properly when desc exists': function () {
+    this.mockFs.expects('readFileSync').returns(new Buffer('sometemplate'));
+    var result = text.format([{ uri: 'http://somehost', status: 'ok', desc: 'somedesc', responseTime: 20 }]);
+    assert.equals(result, 'sometemplate');
+  }
+});
